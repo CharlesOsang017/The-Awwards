a541265d260ab6f081707cd7ad2e32a8a293230e
CharlesOsang017 <charlesosango02@gmail.com>
2020-10-24 13:14:36 +0300

    Initial commit

---
+++ onlyawards/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+import awards
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('awards.links')),
+]
